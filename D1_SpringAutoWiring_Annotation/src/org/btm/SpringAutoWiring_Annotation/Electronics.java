7c5cb2c48f9b878b2ce447211f9658727415f63a
Randhir Prakash <61622146+randhirchhotu7@users.noreply.github.com>
2021-06-23 10:53:22 +0530

    Delete D1_SpringAutoWiring_Annotation directory

--- D1_SpringAutoWiring_Annotation/src/org/btm/SpringAutoWiring_Annotation/Electronics.java
+++
@@ -1,8 +0,0 @@
-package org.btm.SpringAutoWiring_Annotation;
-public class Electronics 
-{
-	void mobile()
-	{
-		System.out.println("Mobile brand is OnePlus !!");
-	}
-}
--- D1_SpringAutoWiring_Annotation/src/org/btm/SpringAutoWiring_Annotation/Furnitures.java
+++
@@ -1,9 +0,0 @@
-package org.btm.SpringAutoWiring_Annotation;
-public class Furnitures 
-{
-	void sofa()
-	{
-		System.out.println("Sofa made by Stanley !!");
-	}
-
-}
